cdb25edb06aa080473d7cde3210265fa93bdf5f5
Kreissys <carlos.llano@tecsup.edu.pe>
2025-10-17 17:40:23 -0600

    Parte 1 terminada con solucion de errores y demas.

---
+++ app/src/main/java/com/example/lab08/data/TaskDatabase.kt
@@ -0,0 +1,10 @@
+package com.example.lab08.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.lab08.model.Task
+
+@Database(entities = [Task::class], version = 1)
+abstract class TaskDatabase : RoomDatabase() {
+    abstract fun taskDao(): TaskDao
+}
