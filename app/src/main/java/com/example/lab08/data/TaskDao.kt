e4f490ac6a0de6a760166d0186cd3dd7e79526a6
Kreissys <carlos.llano@tecsup.edu.pe>
2025-10-17 17:43:28 -0600

    commit proyecto inicial

--- app/src/main/java/com/example/lab08/data/TaskDao.kt
+++ app/src/main/java/com/example/lab08/data/TaskDao.kt
@@ -1,23 +1,4 @@
 package com.example.lab08.data
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
-import com.example.lab08.model.Task
-
-@Dao
-interface TaskDao {
-
-    @Query("SELECT * FROM tasks")
-    suspend fun getAllTasks(): List<Task>
-
-    @Insert
-    suspend fun insertTask(task: Task)
-
-    @Update
-    suspend fun updateTask(task: Task)
-
-    @Query("DELETE FROM tasks")
-    suspend fun deleteAllTasks()
+class TaskDao {
 }
--- app/src/main/java/com/example/lab08/data/TaskDatabase.kt
+++ app/src/main/java/com/example/lab08/data/TaskDatabase.kt
@@ -1,10 +1,4 @@
 package com.example.lab08.data
 
-import androidx.room.Database
-import androidx.room.RoomDatabase
-import com.example.lab08.model.Task
-
-@Database(entities = [Task::class], version = 1)
-abstract class TaskDatabase : RoomDatabase() {
-    abstract fun taskDao(): TaskDao
+class TaskDatabase {
 }
